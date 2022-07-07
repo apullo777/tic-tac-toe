7ed0270888161c1eb451645f0157f7fbc19ad18e
apullo777 <peter.yaochen.chang@gmail.com>
2022-07-07 16:03:27 +0200

    gameplay functionality

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
@@ -7,8 +6,8 @@ const gameBoard = (() => {
     let _board = new Array(9);
     const getField = (num) => _board[num];
     const setField = (num, player) => {
-        const field = document.querySelector(`.field:nth-child(${num + 1})`);
-        field.classList.add('puff-in-center');
+        const field = document.querySelector(`.field:nth-child(${num + 1}) p`);
+        field.classList.add('appear');
         field.textContent = player.getSign();
         _board[num] = player.getSign();
     }
@@ -21,7 +20,7 @@ const gameBoard = (() => {
         _board[num] = player.getSign();
     }
 
-    const getEmptyFieldsIdx = () => {
+    const getAvailableFields = () => {
         fields = [];
         for (let i = 0; i < _board.length; i++) {
             const field = _board[i];
@@ -32,17 +31,17 @@ const gameBoard = (() => {
         return fields;
     }
 
-    const clear = () => {
+    const reset = () => {
         for (let i = 0; i < _board.length; i++) {
             _board[i] = undefined;
         }
     }
     return {
         getField,
-        getEmptyFieldsIdx,
+        getAvailableFields,
         setField,
         setFieldForAiLogic,
-        clear
+        reset
     };
 })();
 
@@ -52,14 +51,14 @@ const Player = (sign) => {
     const getSign = () => _sign;
     const setSign = (sign, active) => {
         _sign = sign;
-        const p = document.querySelector(`.player-btn.${sign.toLowerCase()}`);
+        const player = document.querySelector(`.player-btn.${sign.toLowerCase()}`);
         if(active){
-            p.classList.add('chosen');
-            p.classList.remove('unchosen');
+            player.classList.add('chosen');
+            player.classList.remove('unchosen');
         }
         else{
-            p.classList.remove('chosen');
-            p.classList.add('unchosen');
+            player.classList.remove('chosen');
+            player.classList.add('unchosen');
         }
     }
     return {
@@ -68,9 +67,132 @@ const Player = (sign) => {
     }
 }
 
+
+const minimaxAiLogic = ((percentage) => {
+
+    let threshold = percentage;
+
+    const setAiPercentage = (percentage) => {
+        threshold = percentage;
+    }
+    const getAiPercentage = () => {
+        return threshold;
+    }
+
+
+    const chooseField = () => {
+
+        const value = Math.floor(Math.random() * (100 + 1));
+
+        // if the random number is smaller then the threshold, it finds the best move
+        let choice = null;
+        if (value <= threshold) {
+            console.log('bestChoice');
+            choice = minimax(gameBoard, gameController.getAiPlayer()).index
+            const field = gameBoard.getField(choice);
+            if (field != undefined) {
+                return "error"
+            }
+        }
+        else {
+            console.log('randomChoice');
+            const availableFields = gameBoard.getAvailableFields();
+            let randomMove = Math.floor(Math.random() * availableFields.length);
+            choice = availableFields[randomMove];
+        }
+        return choice;
+    }
+
+
+    const findBestMove = (moves, player) => {
+        let bestMove;
+        if (player === gameController.getAiPlayer()) {
+            let bestScore = -10000;
+            for (let i = 0; i < moves.length; i++) {
+                if (moves[i].score > bestScore) {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        } else {
+            let bestScore = 10000;
+            for (let i = 0; i < moves.length; i++) {
+                if (moves[i].score < bestScore) {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        }
+        return moves[bestMove];
+
+    }
+
+
+    const minimax = (newBoard, player) => {
+
+        let availableFields = newBoard.getAvailableFields();
+
+        if (gameController.checkDraw(newBoard)) {
+            return {
+                score: 0
+            };
+        }
+        else if (gameController.checkWinner(newBoard)) {
+
+            if (player.getSign() == gameController.getHumanPlayer().getSign()) {
+                return {
+                    score: 10
+                };
+            }
+            else if (player.getSign() == gameController.getAiPlayer().getSign()) {
+                return {
+                    score: -10
+                };
+            }
+        }
+
+        let availableMoves = [];
+
+        for (let i = 0; i < availableFields.length; i++) {
+            let move = {};
+            move.index = availableFields[i];
+
+            //Change the field value to the sign of the player
+            newBoard.setFieldForAiLogic(availableFields[i], player);
+
+            //Call the minimax with the opposite player
+            if (player.getSign() == gameController.getAiPlayer().getSign()) {
+                let result = minimax(newBoard, gameController.getHumanPlayer());
+                move.score = result.score;
+            }
+            else {
+                let result = minimax(newBoard, gameController.getAiPlayer());
+                move.score = result.score;
+            }
+
+            //Reset the fieled values
+            newBoard.setFieldForAiLogic(availableFields[i], undefined);
+
+            availableMoves.push(move);
+        }
+
+        //find the best move
+        return findBestMove(availableMoves, player);
+
+    }
+    return {
+        minimax,
+        chooseField,
+        getAiPercentage,
+        setAiPercentage
+    }
+})(0);
+
 const gameController = (() => {
     const _humanPlayer = Player('X');
     const _aiPlayer = Player('O')
+    const _aiLogic = minimaxAiLogic;
+
 
     const getHumanPlayer = () => _humanPlayer;
     const getAiPlayer = () => _aiPlayer;
@@ -119,15 +241,15 @@ const gameController = (() => {
         return false;
     }
 
-    const checkForWin = (board) => {
+    const checkWinner = (board) => {
         if (_checkForRows(board) || _checkForColumns(board) || _checkForDiagonals(board)) {
             return true;
         }
         return false;
     }
 
-    const checkForDraw = (board) => {
-        if (checkForWin(board)) {
+    const checkDraw = (board) => {
+        if (checkWinner(board)) {
             return false;
         }
         for (let i = 0; i < 9; i++) {
@@ -139,8 +261,7 @@ const gameController = (() => {
         return true;
     }
 
-
-    const changeSign = (sign) => {
+    const switchSign = (sign) => {
         if (sign == 'X') {
             _humanPlayer.setSign('X', true);
             _aiPlayer.setSign('O');
@@ -156,31 +277,49 @@ const gameController = (() => {
         const field = gameBoard.getField(num);
         if (field == undefined) {
             gameBoard.setField(num, _humanPlayer);
-            if (checkForWin(gameBoard)) {
+            if (checkWinner(gameBoard)) {
                 (async () => {
                     await _sleep(500 + (Math.random() * 500));
                     endGame(_humanPlayer.getSign());
                 })();  
             }
-            else if (checkForDraw(gameBoard)) {
+            else if (checkDraw(gameBoard)) {
                 (async () => {
                     await _sleep(500 + (Math.random() * 500));
                     endGame("Draw");
                 })();  
             }
             else {
-                displayController.deactivate();
+                displayController.closeGame();
                 (async () => {
                     await _sleep(250 + (Math.random() * 300));
                     aiStep();
-                    if (!checkForWin(gameBoard)) {
-                        displayController.activate();
+                    if (!checkWinner(gameBoard)) {
+                        displayController.openGame();
                     }
                 })();
             }
         }
         else {
             console.log('Already Filled')
+        }
+    }
+
+    const aiStep = () => {
+        const num = _aiLogic.chooseField();
+        gameBoard.setField(num, _aiPlayer);
+        if (checkWinner(gameBoard)) {
+            (async () => {
+                await _sleep(500 + (Math.random() * 500));
+                endGame(_aiPlayer.getSign())
+            })();  
+            
+        }
+        else if (checkDraw(gameBoard)) {
+            (async () => {
+                await _sleep(500 + (Math.random() * 500));
+                endGame("Draw");
+            })();  
         }
     }
 
@@ -206,9 +345,9 @@ const gameController = (() => {
                 winElements[2].classList.remove('hide');
             }
         }
-        console.log('deactivate');
-        displayController.deactivate();
-        displayController.makeBodyRestart();
+        console.log('close game');
+        displayController.closeGame();
+        displayController.handleRestart();
     }
 
 
@@ -219,17 +358,17 @@ const gameController = (() => {
 
         container.classList.add('unblur');
 
-        gameBoard.clear();
-        displayController.clear();
+        gameBoard.reset();
+        displayController.reset();
         if (_humanPlayer.getSign() == 'O') {
             aiStep();
         }
         console.log('restart');
         console.log(minimaxAiLogic.getAiPercentage());
-        displayController.activate();
+        displayController.openGame();
 
    
-        card.classList.remove('blur');
+        container.classList.remove('blur');
       
         winElements.forEach(element => {
             element.classList.add('hide');
@@ -241,9 +380,9 @@ const gameController = (() => {
     return {
         getHumanPlayer,
         getAiPlayer,
-        checkForWin,
-        checkForDraw,
-        changeSign,
+        checkWinner,
+        checkDraw,
+        switchSign,
         playerStep,
         endGame,
         restart
@@ -251,22 +390,22 @@ const gameController = (() => {
 })();
 
 const displayController = (() => {
-    const htmlBoard = Array.from(document.querySelectorAll('button.field'));
+    const board = Array.from(document.querySelectorAll('.field'));
     const form = document.querySelector('.form');
     const restart = document.querySelector('.restart');
     const x = document.querySelector('.x');
     const o = document.querySelector('.o');
 
-    const _changeAI = () => {
+    const _switchAI = () => {
         const value = document.querySelector('#levels').value;
         if (value == 'easy') {
             minimaxAiLogic.setAiPercentage(0);
         }
         else if (value == 'medium') {
-            minimaxAiLogic.setAiPercentage(75);
+            minimaxAiLogic.setAiPercentage(65);
         }
         else if (value == 'hard') {
-            minimaxAiLogic.setAiPercentage(90);
+            minimaxAiLogic.setAiPercentage(80);
         }
         else if (value == 'unbeatable') {
             minimaxAiLogic.setAiPercentage(100);
@@ -274,57 +413,59 @@ const displayController = (() => {
         gameController.restart();
     }
 
-    const _changePlayerSign = (sign) => {
-        gameController.changeSign(sign);
+    const _switchPlayerSign = (sign) => {
+        gameController.switchSign(sign);
         gameController.restart();
     }
 
-    const clear = () => {
-        htmlBoard.forEach(field => {
+    const reset = () => {
+        board.forEach(field => {
             const p = field.childNodes[0];
             p.classList = [];
             p.textContent = '';
         });
     }
 
-    const deactivate = () => {
-        htmlBoard.forEach(field => {
+    const closeGame = () => {
+        board.forEach(field => {
             field.setAttribute('disabled', '');
         });
     }
 
-    const activate = () => {
-        htmlBoard.forEach(field => {
+    const openGame = () => {
+        board.forEach(field => {
 
             field.removeAttribute('disabled');
         });
     }
 
-    const makeBodyRestart = () =>{
+    const handleRestart = () =>{
         const body = document.querySelector('body');
         body.addEventListener('click', gameController.restart);
         
     }
 
     const _init = (() => {
-        for (let i = 0; i < htmlBoard.length; i++) {
-            field = htmlBoard[i];
+        for (let i = 0; i < board.length; i++) {
+            field = board[i];
             field.addEventListener('click', gameController.playerStep.bind(field, i));
         }
 
-        form.addEventListener('change', _changeAI);
+        form.addEventListener('change', _switchAI);
 
         restart.addEventListener('click', gameController.restart);
 
-        x.addEventListener('click', _changePlayerSign.bind(this, 'X'));
+        x.addEventListener('click', _switchPlayerSign.bind(this, 'X'));
 
-        o.addEventListener('click', _changePlayerSign.bind(this, 'O'));
+        o.addEventListener('click', _switchPlayerSign.bind(this, 'O'));
     })();
 
     return {
-        deactivate,
-        activate,
-        clear,
-        makeBodyRestart
+        closeGame,
+        openGame,
+        reset,
+        handleRestart
     }
 })();
+
+gameController.switchSign('X')
